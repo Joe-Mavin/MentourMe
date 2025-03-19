2fc2f34b9348a0226c7946f8daa9550f5ae7b59d
Joe-mavin <mavinodundo@gmail.com>
2025-03-19 12:53:38 +0300

    database config

--- Server/src/config/database.mjs
+++ Server/src/config/database.mjs
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: "mariadb",
-  dialectOptions: {
-    ssl: false,
-    allowPublicKeyRetrieval: true,
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "your_database_name",
+  process.env.DB_USER || "root",
+  process.env.DB_PASS || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mariadb",
+    dialectOptions: {
+      ssl: false,
+      allowPublicKeyRetrieval: true,
+    },
+    logging: false, // Disables query logging
   }
-});
+);
 
 export default sequelize;
