d56f35b51dd8ea5d2658f408d4626f04d28d0f49
Joe-mavin <mavinodundo@gmail.com>
2025-03-20 17:23:19 +0300

    update

--- Server/server.mjs
+++ Server/server.mjs
@@ -19,6 +19,6 @@ const PORT = process.env.PORT || 5001;
 sequelize.sync()
   .then(() => {
     console.log("Database connected ✅");
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`Server running on all interfaces (0.0.0.0:5001)`));
   })
   .catch((err) => console.error("DB Connection Error ❌", err));
