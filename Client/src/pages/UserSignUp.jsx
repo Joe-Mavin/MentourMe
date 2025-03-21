ff58100384ac878caf0827ba02e10981899aafa0
Joe-mavin <mavinodundo@gmail.com>
2025-03-21 15:06:46 +0300

    update

--- Client/src/pages/UserSignUp.jsx
+++ Client/src/pages/UserSignUp.jsx
@@ -55,7 +55,7 @@ const SignUpPage = () => {
     setIsLoading(true);
     try {
       const response = await axios.post(
-        "http://localhost:5001/api/auth/signup",
+        "/api/auth/signup",
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
