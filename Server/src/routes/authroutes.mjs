d4bb0a1a886a032d7994c142ef3fdd6527bc976a
Joe-mavin <mavinodundo@gmail.com>
2025-03-21 12:34:33 +0300

    fix

--- Server/src/routes/authroutes.mjs
+++ Server/src/routes/authroutes.mjs
@@ -1,10 +1,9 @@
 import express from "express";
-import { signup,getUsers } from "../controllers/authController.mjs";
+import { signup} from "../controllers/authController.mjs";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/users", getUsers);
 
 
 export default router;
