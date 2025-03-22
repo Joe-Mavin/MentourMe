e1da4080649abfbfd60bb40dc2f0cebce36bc5a3
Joe-mavin <mavinodundo@gmail.com>
2025-03-22 10:50:55 +0300

    update my api request route

--- Client/src/pages/UserSignUp.jsx
+++ Client/src/pages/UserSignUp.jsx
@@ -55,7 +55,7 @@ const SignUpPage = () => {
     setIsLoading(true);
     try {
       const response = await axios.post(
-        "/auth/signup",
+        "http://testmehere.onyangojp.tech:8080/api/auth/signup",
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
